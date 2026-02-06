c97edc74d122e2404f707d42e5ad6a7e97ecfaa1
= <=>
2026-02-06 14:25:40 +0000

    Add deployment script for Meeting Bot to build and push Docker image

---
+++ DeployBot.ps1
@@ -0,0 +1,4 @@
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 391202570628.dkr.ecr.eu-west-1.amazonaws.com
+docker build --pull --rm -f 'Dockerfile.production' -t 'meeting-bot:latest' '.'
+docker tag meeting-bot:latest 391202570628.dkr.ecr.eu-west-1.amazonaws.com/meeting-bot:latest
+docker push 391202570628.dkr.ecr.eu-west-1.amazonaws.com/meeting-bot:latest
