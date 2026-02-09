497af52a88284b76aaf5773df54233eb9c78f77a
= <=>
2026-02-09 09:57:22 +0000

    deploy

--- DeployBot.ps1
+++ DeployBot.ps1
@@ -2,3 +2,6 @@ aws ecr get-login-password --region eu-west-1 | docker login --username AWS --pa
 docker build --pull --rm -f 'Dockerfile.production' -t 'meeting-bot:latest' '.'
 docker tag meeting-bot:latest 391202570628.dkr.ecr.eu-west-1.amazonaws.com/meeting-bot:latest
 docker push 391202570628.dkr.ecr.eu-west-1.amazonaws.com/meeting-bot:latest
+$env:AWS_PAGER = ""
+aws ecs update-service --no-cli-pager --cluster helloworld3-cluster --service meeting-bot-task-service-venno6jl  --force-new-deployment | Out-Null
+
