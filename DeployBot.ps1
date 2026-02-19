b775879175e8268ae190eb513c33353a6a746a40
= <=>
2026-02-19 13:34:35 +0000

    deploy script

--- DeployBot.ps1
+++ DeployBot.ps1
@@ -3,8 +3,7 @@ docker build --pull --rm -f 'Dockerfile.production' -t 'meeting-bot:latest' '.'
 docker tag meeting-bot:latest 391202570628.dkr.ecr.eu-west-1.amazonaws.com/meeting-bot:latest
 docker push 391202570628.dkr.ecr.eu-west-1.amazonaws.com/meeting-bot:latest
 $env:AWS_PAGER = ""
-#aws ecs update-service --no-cli-pager --cluster helloworld3-cluster --service meeting-bot-task-service-venno6jl  --force-new-deployment | Out-Null
-aws ecs update-service --no-cli-pager --cluster helloworld3-cluster --service meeting-bot-1  --force-new-deployment | Out-Null
-aws ecs update-service --no-cli-pager --cluster helloworld3-cluster --service meeting-bot-2  --force-new-deployment | Out-Null
-aws ecs update-service --no-cli-pager --cluster helloworld3-cluster --service meeting-bot-3  --force-new-deployment | Out-Null
+
+aws ecs update-service --no-cli-pager --cluster helloworld3-cluster --service meeting-bot-pool --force-new-deployment | Out-Null
+
 
